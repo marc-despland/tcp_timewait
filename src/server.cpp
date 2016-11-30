5ea4be3b1cdfff1b2521d67e4f73484acaf35829
Marc Despland <marc.despland@art122-5.net>
2016-11-30 11:34:39 +0100

    usleep is microsecond, so change the wait duration to 200 for server mode 9

--- src/server.cpp
+++ src/server.cpp
@@ -218,7 +218,7 @@ void Server::run(int scenario) {
 		case 9:
 			this->state->is_an_http_server=true;
 			this->state->http_close_after_response=true;
-			this->state->short_close_wait=1000;
+			this->state->short_close_wait=200;
 		break;
 	}
 	this->scenario=scenario;
